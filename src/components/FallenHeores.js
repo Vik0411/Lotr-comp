dc0f7ef0fc14bb0c69309528311d1f31b41ebc07
Vik <svoboda411@gmail.com>
2023-07-10 19:23:51 +0200

    comment code to make it more undestandable

--- src/components/FallenHeores.js
+++ src/components/FallenHeores.js
@@ -3,19 +3,23 @@ import { allHerosMock, state } from "../context";
 import { persistData } from "../context";
 
 function FallenHeroes(){
-
-    const addFallenHero = function(hero){
+       
+    const addFallenHero = function(e){
+        var value = e.target.value
+        console.log(value)
+        const fname = value;
+        console.log(fname)
         //add hero to the list if it isnt there yet
-        if(!state.restOfAliveHeroes.includes(hero)){
+        if(!state.restOfAliveHeroes.includes(fname)){
             throw Error
         } 
-        if(!allHerosMock.includes(hero) && state.restOfAliveHeroes.includes(hero)){
-            state.fallenHeroes.push(hero);
-            delete state.restOfAliveHeroes[hero]
+        if(allHerosMock.includes(fname) && state.restOfAliveHeroes.includes(fname)){
+            state.fallenHeroes.push(fname);
+            delete state.restOfAliveHeroes[fname]
             persistData();
             console.log(state)
         }
-        if(!allHerosMock.includes(hero)){
+        if(!allHerosMock.includes(fname)){
             throw Error
         } 
 
@@ -24,15 +28,14 @@ function FallenHeroes(){
     return (
         <div>
     <header class="header">
-        <input type="text" class="input_fallen" placeholder="Input your fallen heroes..." onSubmit={addFallenHero}
-
+        <input type="text" className="input_fallen" placeholder="Input your fallen heroes..."
         />   
-            <button class="btn input_fallen" >Send to the coffin</button>
+            <button class="btn input_fallen" onClick={addFallenHero}>Send to the coffin</button>
     </header>
         <header class="header">
             <button class="btn list_fallen">List fallen heroes</button>
                 <div class="fallen__heroes__panel">
-                    <ul class="fallen_heroes__list">  
+                    <ul class="fallen_heroes__list">
                     <FallenHero/>
                     </ul>
                 </div>
@@ -42,3 +45,7 @@ function FallenHeroes(){
 }
 
 export default FallenHeroes
+
+
+
+
--- src/context.js
+++ src/context.js
@@ -1,3 +1,6 @@
+// here the first working version with local browser storage was only in javascript
+// and will have to be adjusted to react and useState() hooks probably
+// This page should work as mainly as the data handling place
 
 export const allHerosMock = ['Aragorn', 'Eowyn', 'Beravor']
 export const state = {
@@ -5,22 +8,14 @@ export const state = {
     restOfAliveHeroes: ['Eowyn', 'Beravor'],
     allHeros: allHerosMock
 }
+
 export const persistData = function () {
+    // I believe we could simply use this browser local storage for our purposes
     localStorage.setItem('fallenHeroes', JSON.stringify(state.fallenHeroes))
 }
 
-
-export const addFallenHero = function(hero){
-    //add hero to the list if it isnt there yet
-    if(state.restOfAliveHeroes.includes(hero)){
-        throw Error
-    } else {
-        state.fallenHeroes.push(hero);
-        delete state.restOfAliveHeroes[hero]
-        persistData();
-    }   
-}
-
+// unfinished function for potentianlly new feature of also keeping count
+// of individual deaths for the heroes
 export const addFallenHeroCount = function(hero){
     //add hero to the list if it isnt there yet
     if(state.restOfAliveHeroes.includes(hero)){
@@ -30,6 +25,8 @@ export const addFallenHeroCount = function(hero){
         persistData();
     }   
 }
+
+// function preparation for reseting those heroes remaining alive
 export const resetRestOfHeroes = function(hero){
     //add hero to the list if it isnt there yet
     if(state.restOfAliveHeroes.includes(hero)){
@@ -40,7 +37,6 @@ export const resetRestOfHeroes = function(hero){
     }   
 }
 
-const fallenHeroes = "y";
 
 const init = function(){
     const storage = localStorage.getItem('fallenHeroes')
