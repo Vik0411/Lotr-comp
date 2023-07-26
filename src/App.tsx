cfaf8f934a544f1d6f6db66581c98d457abe82a7
Vik <svoboda411@gmail.com>
2023-07-26 21:24:24 +0200

    Move rest stuff from App to components and other formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import FallenHeroes from "./components/FallenHeroes";
 import { LOTRProvider } from "./context";
+import AllMightyHeroes from "./components/AllMightyHeroes";
+import Survivors from "./components/Survivors";
 
 function App() {
   return (
     <LOTRProvider>
-      <div className="App">
-        <header className="App-header">
-          <header className="header">
-            <button className="btn list_all">List all the mighty heroes</button>
-            <div className="all__heroes__panel">
-              <ul className="all_heroes__list">all heroes list</ul>
-            </div>
-          </header>
-          <FallenHeroes />
-          <header className="header">
-            <button className="btn list_survivors">List survivors</button>
-            <div className="rest__panel">
-              <ul className="rest__heroes__list">{}</ul>
-            </div>
-          </header>
-          <img />
-          <p>
-            <code></code>
-          </p>
-          <a></a>
-        </header>
-      </div>
+      <AllMightyHeroes />
+      <FallenHeroes />
+      <Survivors />
     </LOTRProvider>
   );
 }
