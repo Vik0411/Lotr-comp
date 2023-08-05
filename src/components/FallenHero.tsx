69cd33c556c5020ac5ee421d5d9da7e69095e5b0
Vik <svoboda411@gmail.com>
2023-08-05 15:32:09 +0200

    revert mistaken copypaste into FallenHero

--- src/components/FallenHero.tsx
+++ src/components/FallenHero.tsx
@@ -1,67 +1,25 @@
-import { LotrContext } from "../context";
-import React, { useState } from "react";
-
-function FallenHeroes() {
-  // the FallenHero component should be rendered in this component in a loop or so
-  // to show all the fallen heroes, via forEach() or so
-
-  // addFallenHero() - unfinished function for the send to coffin button
-  // to get the hero from the input text and if correct, that is
-  // matching hero from the set of totalHeroes,
-  // and is included in rest of heroes alive, update the state accordingly
-  const { campaign, setCampaign } = React.useContext(LotrContext);
-  const [fallenHero, setFallenHero] = useState("");
-
-  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
-    e.preventDefault();
-    setFallenHero(e.target.value);
-  };
-
-  function addFallenHero(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    //add hero to the list if it isnt there yet
-    if (!campaign.restOfAliveHeroes.includes(fallenHero)) {
-      throw Error;
-    }
-    if (campaign.restOfAliveHeroes.includes(fallenHero)) {
-      const updatedFallen = [...campaign.fallenHeroes, fallenHero];
-      const updatedRestOfAlive = campaign.restOfAliveHeroes.filter(
-        (hero) => hero !== fallenHero
-      );
-      setCampaign({
-        ...campaign,
-        fallenHeroes: updatedFallen,
-        restOfAliveHeroes: updatedRestOfAlive,
-      });
-    }
-    if (!campaign.allHeroes.includes(fallenHero)) {
-      throw Error;
-    }
-  }
-
-  console.log(campaign);
-  return (
-    <div>
-      <form onSubmit={addFallenHero}>
-        <input
-          type="text"
-          className="input_fallen"
-          value={fallenHero}
-          placeholder="Input your fallen hero..."
-          onChange={handleChange}
-        />
-        <button type="submit" className="btn input_fallen">
-          Send to the coffin
-        </button>
-        <header className="header">
-          <button className="btn list_fallen">List fallen heroes</button>
-          <div className="fallen__heroes__panel">
-            <ul className="fallen_heroes__list">{campaign.fallenHeroes}</ul>
-          </div>
-        </header>
-      </form>
-    </div>
-  );
+/* this is only optional html template for use
+export function FallenHero(){
+return (
+  <li>
+   <a class="hero__link" href="#23456">
+       <figure class="hero__fig">
+           <img src="img/test-1.jpg" alt="Test"/>
+       </figure>
+       <div class="hero__data">
+           <h4 class="hero__name">Default name</h4>
+           <p class="hero__type">Default type as Spirit</p>
+       </div>
+   </a>
+</li>
+)
 }
+*/
 
-export default FallenHeroes;
+function FallenHero() {
+  // this component is for rendering individual fallen hero, eventually
+  // also with more data, such as scenarios played, image etc.
+  // const {fallenHeroes} = React.useContext(GithubContext);
+
+  return <div className="fallen"> </div>;
+}
