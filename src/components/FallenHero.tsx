6057ca7399c9fe2e2309b52ca0f13228a89f21c3
Viktor Svoboda <viktor.svoboda@erstegroup.com>
2023-11-07 12:20:35 +0100

    remove unnecessary comments

--- src/components/FallenHero.tsx
+++ src/components/FallenHero.tsx
@@ -1,21 +1,3 @@
-/* this is only optional html template for use
-export function FallenHero(){
-return (
-  <li>
-   <a class="hero__link" href="#23456">
-       <figure class="hero__fig">
-           <img src="img/test-1.jpg" alt="Test"/>
-       </figure>
-       <div class="hero__data">
-           <h4 class="hero__name">Default name</h4>
-           <p class="hero__type">Default type as Spirit</p>
-       </div>
-   </a>
-</li>
-)
-}
-*/
-
 export function FallenHero({ fallenHero }: { fallenHero: string }) {
   // this component is for rendering individual fallen hero, eventually
   // also with more data, such as scenarios played, image etc.
