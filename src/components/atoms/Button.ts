0e2f8b2cdf36c82d35504463db3b158a24f50202
Vik <svoboda411@gmail.com>
2023-11-30 10:51:25 +0100

    remove min width from Button

--- src/components/atoms/Button.ts
+++ src/components/atoms/Button.ts
@@ -3,7 +3,6 @@ import styled from "styled-components";
 export const Button = styled.button`
   border-radius: 50px;
   border: none;
-  min-width: 10vw;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.4);
   cursor: pointer;
   font-size: 16px;
