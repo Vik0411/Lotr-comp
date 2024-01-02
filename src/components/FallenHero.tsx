73c45f2f97c709a02810671298ed4b09bc2a4092
Vik <svoboda411@gmail.com>
2024-01-02 08:49:06 +0100

    add resurrect feature

--- src/components/FallenHero.tsx
+++ src/components/FallenHero.tsx
@@ -1,13 +1,32 @@
+import React from "react";
 import { Hero } from "../types";
 import { CancelBtn } from "./atoms/Button";
 import { CancelImage } from "./atoms/CancelImage";
 import { ListItemWithWhiteText } from "./atoms/ListItemWithWhiteText";
+import { LotrContext } from "../context";
 
-export function FallenHero({ name }: Hero) {
+export function FallenHero({ name, code }: Hero) {
+  const { campaign, setCampaign } = React.useContext(LotrContext);
+
+  function resurrectHero(heroCode: string) {
+    // add alert when one "duplicate" is already killed
+    setCampaign({
+      ...campaign,
+      allHeroes: campaign.allHeroes.map((hero) => {
+        if (hero.code === heroCode) {
+          hero.alive = true;
+          hero.current = true;
+          return hero;
+        } else {
+          return hero;
+        }
+      }),
+    });
+  }
   return (
     <ListItemWithWhiteText>
       {name}
-      <CancelBtn>
+      <CancelBtn onClick={() => resurrectHero(code)}>
         <CancelImage
           alt=""
           src={require("../images/cancel-1.png")}
