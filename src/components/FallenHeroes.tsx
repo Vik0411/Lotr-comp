a72b27b348fb2c843ba15f38c56b5930651f983c
Vik <svoboda411@gmail.com>
2024-02-20 11:02:42 +0100

    clear fallenHeroes

--- src/components/FallenHeroes.tsx
+++ src/components/FallenHeroes.tsx
@@ -3,14 +3,6 @@ import { LotrContext } from "../context";
 import { Hero } from "../types";
 import { filterHeroes } from "../utils";
 import { FallenHero } from "./FallenHero";
-import { SectionHeader } from "./atoms/typography";
-import { styled } from "styled-components";
-import { ContainerFallenHeroes } from "./atoms/Containers";
-
-const BottomSectionHeader = styled(SectionHeader)`
-  margin-top: -30px;
-  margin-bottom: 10px;
-`;
 
 function FallenHeroes() {
   const { campaign } = React.useContext(LotrContext);
