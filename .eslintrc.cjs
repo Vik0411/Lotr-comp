e020cdd62b39fcb4d79fec5a896d01c4260c14e7
Viktor Svoboda <viksvoboda@proton.me>
2024-03-13 14:44:43 +0100

    historyApi fallback

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,6 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  devServer: { historyApiFallback: true },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
